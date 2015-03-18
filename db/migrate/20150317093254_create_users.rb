2bcac42e667cd8e05b721209259566d658b52ebb
Thuy Anh <nguyen.thuy.anh@framgia.com>
2015-03-18 15:32:08 +0700

    Finish users function with gem devise

--- db/migrate/20150317093254_create_users.rb
+++ db/migrate/20150317093254_create_users.rb
@@ -2,10 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
-      t.string :password_digest
-      t.boolean :admin
-      t.string :remember_digest
+      t.boolean :admin, default: false 
 
       t.timestamps null: false
     end
